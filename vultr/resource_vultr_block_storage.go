8932022f4124d8962bff2b5694d73fe0abd7b901
Sourcegraph <batch-changes@sourcegraph.com>
2023-03-18 00:32:07 +0000

    Replacing log package with logrus

--- vultr/resource_vultr_block_storage.go
+++ vultr/resource_vultr_block_storage.go
@@ -3,7 +3,7 @@ package vultr
 import (
 	"context"
 	"fmt"
-	"log"
+	log "github.com/sourcegraph-ce/logrus"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
--- vultr/resource_vultr_iso_private.go
+++ vultr/resource_vultr_iso_private.go
@@ -3,7 +3,7 @@ package vultr
 import (
 	"context"
 	"fmt"
-	"log"
+	log "github.com/sourcegraph-ce/logrus"
 	"strconv"
 	"time"
 
